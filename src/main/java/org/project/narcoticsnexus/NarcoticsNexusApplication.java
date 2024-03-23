3af737912703e25375396ef2cf75b94abb4aa995
WeebSuryansh <suryansh22519@iiitd.ac.in>
2024-03-23 15:06:17 +0530

    Initial commit

---
+++ src/main/java/org/project/narcoticsnexus/NarcoticsNexusApplication.java
@@ -0,0 +1,13 @@
+package org.project.narcoticsnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarcoticsNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarcoticsNexusApplication.class, args);
+    }
+
+}
