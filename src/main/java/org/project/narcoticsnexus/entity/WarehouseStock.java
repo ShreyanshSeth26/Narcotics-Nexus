10c4b1ef01c05beddf74d7d32d68941d1af60db1
WeebSuryansh <suryansh22519@iiitd.ac.in>
2024-03-24 20:48:06 +0530

    final entities and enum added

---
+++ src/main/java/org/project/narcoticsnexus/entity/WarehouseStock.java
@@ -0,0 +1,21 @@
+package org.project.narcoticsnexus.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class WarehouseStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long stockId;
+    @ManyToOne
+    private Warehouse warehouse;
+    @ManyToOne
+    private Product product;
+    private int stock;
+}
